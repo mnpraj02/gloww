ce1559a9df65aa321e7b6a3dbfbb9dcfc67b953c
mnpraj02 <zinga9894@gmail.com>
2023-06-13 19:05:50 +0530

    mmm

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const port = process.env.PORT || 5000
+
+
+
+app.get('/' , (req , res)=>{
+
+   res.send('hello from simple server this is prudhvi :)')
+
+})
+
+
+app.listen(port , ()=> console.log('> Server is up and running on port : ' + port))
